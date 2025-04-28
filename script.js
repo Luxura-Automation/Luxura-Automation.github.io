6a53775cf45fca8349bbba58b1c14be06ebc6503
Luxura-Automation <LuxuraAutomation@gmail.com>
2025-04-28 22:35:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Tooltip
+function showTooltip(text) {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.innerHTML = text;
+    tooltip.style.display = 'block';
+    document.onmousemove = function (e) {
+        tooltip.style.left = (e.pageX + 10) + 'px';
+        tooltip.style.top = (e.pageY + 10) + 'px';
+    }
+}
 
+function hideTooltip() {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.display = 'none';
+}
+
+// Modal
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
